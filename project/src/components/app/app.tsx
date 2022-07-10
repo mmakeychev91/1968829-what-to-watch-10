3b28cdce69689870008f46b6dd2ae160e73ab0a3
Михаил Макейчев <mihailmakejcev@MacBook-Air-Mihail.local>
2022-07-10 23:54:28 +0300

    2.9. Начинаем программировать

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,26 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainScreen from '../../pages/mainScreen/mainScreen';
+
+type AppScreenProps = {
+  filmCardAmountPerStep: number;
+  filmName: string;
+  filmGenre: string;
+  filmReliseDate: number;
+};
+
+function App({
+  filmCardAmountPerStep,
+  filmName,
+  filmGenre,
+  filmReliseDate,
+}: AppScreenProps): JSX.Element {
+  return (
+    <MainScreen
+      filmCardAmountPerStep={filmCardAmountPerStep}
+      filmName={filmName}
+      filmGenre={filmGenre}
+      filmReliseDate={filmReliseDate}
+    />
+  );
 }
 
 export default App;
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,12 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+const Setting = {
+  FILM_CARDS_AMOUNT_PER_STEP: 20,
+};
+
+const filmCardsData = {
+  filmName: 'The Grand Budapest Hotel',
+  filmGenre: 'Drama',
+  filmReliseDate: 2014,
+};
+
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement,
+  document.getElementById('root') as HTMLElement
 );
 
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App
+      filmCardAmountPerStep={Setting.FILM_CARDS_AMOUNT_PER_STEP}
+      filmName={filmCardsData.filmName}
+      filmGenre={filmCardsData.filmGenre}
+      filmReliseDate={filmCardsData.filmReliseDate}
+    />
+  </React.StrictMode>
 );
