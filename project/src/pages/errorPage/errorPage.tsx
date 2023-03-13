82348c8075919605e0168ef2cba068b4cec02456
mmakeychev91 <mmakejcev@gmail.com>
2023-03-13 13:00:07 +0300

    fix

--- project/src/pages/errorPage/errorPage.tsx
+++ project/src/pages/errorPage/errorPage.tsx
@@ -1,6 +1,8 @@
+import React from 'react';
+
 const errorPage = (): JSX.Element => (
   <>
-    <a href="/">Вернуться на глвную</a>
+    <a href="/">Вернуться на главную</a>
     <h1>404 Not Found</h1>
   </>
 );
